283af5d6652a001b7eca9ab81724d3dba7fe117a
AsimMahmudov <mahmudovasim799@gmail.com>
2025-02-18 09:35:42 +0600

    сделал ненмного поправок

--- src/components/pages/home/catalog/Catalog.tsx
+++ src/components/pages/home/catalog/Catalog.tsx
@@ -1,39 +1,46 @@
 import { GiBunkBeds } from "react-icons/gi";
 import scss from "./Catalog.module.scss";
+import { LuSofa } from "react-icons/lu";
+import { TbArmchair } from "react-icons/tb";
+import { PiLockersBold } from "react-icons/pi";
+import { BiCabinet } from "react-icons/bi";
+import { MdOutlineTableBar } from "react-icons/md";
 
 const Catalog = () => {
 	const data = [
 		{
-			icon: <GiBunkBeds />,
-			title: "Кровати",
+			icon: <LuSofa />,
+			title: "Диваны",
+		},
+		{
+			icon: <TbArmchair />,
+			title: "Кресла",
+		},
+
+		{
+			icon: <PiLockersBold />,
+			title: "Шкафы",
+		},
+		{
+			icon: <BiCabinet />,
+			title: "Тумбы",
+		},
+		{
+			icon: <MdOutlineTableBar />,
+			title: "Столы",
 		},
 		{
 			icon: <GiBunkBeds />,
-			title: "Кровати",
+			title: "Кровать",
 		},
 		{
 			icon: <GiBunkBeds />,
-			title: "Кровати",
+			title: "Матрасы",
 		},
+
 		{
 			icon: <GiBunkBeds />,
-			title: "Кровати",
-		},
-		{
-			icon: <GiBunkBeds />,
-			title: "Кровати",
-		},
-		{
-			icon: <GiBunkBeds />,
-			title: "Кровати",
-		},
-		{
-			icon: <GiBunkBeds />,
-			title: "Кровати",
-		},
-		{
-			icon: <GiBunkBeds />,
-			title: "Кровати",
+			title: "ковры",
 		},
 	];
 	return (
@@ -44,7 +51,7 @@ const Catalog = () => {
 				<div className={scss.content}>
 					{data.map((el, index) => (
 						<div key={index} className={scss.card}>
-							 <h1>{el.icon}</h1>
+							<h1>{el.icon}</h1>
 							<h2>{el.title}</h2>
 						</div>
 					))}
--- src/components/ui/card/Card.tsx
+++ src/components/ui/card/Card.tsx
@@ -9,31 +9,30 @@ const Card = () => {
 	return (
 		<>
 			{use_data.map((el, index) => (
-				<Link key={index} href={`/${el.id}`}>
-					<div   className={scss.Card}>
-						<div className={scss.content}>
-							<div className={scss.image}>
+				<div key={index} className={scss.Card}>
+					<div className={scss.content}>
+						<div className={scss.image}>
+							<Link href={`/${el.id}`}>
 								<Image fill objectFit="cover" src={el.main_image} alt="img" />
-
-								<button>
-									<FaRegHeart />
-								</button>
+							</Link>
+							<button>
+								<FaRegHeart />
+							</button>
+						</div>
+						<div className={scss.box}>
+							<h1>{el.title}</h1>
+							<div className={scss.price}>
+								<p className={scss.price1}>
+									<s>{el.old_price} c</s>
+								</p>
+								<p className={scss.price2}>{el.new_price} c</p>
 							</div>
-							<div className={scss.box}>
-								<h1>{el.title}</h1>
-								<div className={scss.price}>
-									<p className={scss.price1}>
-										<s>{el.old_price} c</s>
-									</p>
-									<p className={scss.price2}>{el.new_price} c</p>
-								</div>
-								<button>
-									<TiShoppingCart />В корзину
-								</button>
-							</div>
+							<button>
+								<TiShoppingCart />В корзину
+							</button>
 						</div>
 					</div>
-				</Link>
+				</div>
 			))}
 		</>
 	);
