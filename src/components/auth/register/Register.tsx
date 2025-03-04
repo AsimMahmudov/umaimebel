089cc071a6531c6864ac7f7e12180a7d68840085
Marlen <zomadujsonov@gmail.com>
2025-03-04 14:09:08 +0600

    auth

--- src/components/auth/register/Register.tsx
+++ src/components/auth/register/Register.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { usePostRegisterUserMutation } from "@/redux/api/auth";
+import axios from "axios";
 
 interface RegisterType {
+  user_image: string;
+  phone_number: string;
   username: string;
   email: string;
   password: string;
@@ -10,14 +13,39 @@ interface RegisterType {
 
 const Register = () => {
   const { register, handleSubmit } = useForm<RegisterType>();
-  const [postRegisterUser, { isLoading, error }] = usePostRegisterUserMutation();
+  const [postRegisterUser] = usePostRegisterUserMutation();
 
   const onSubmit: SubmitHandler<RegisterType> = async (data) => {
+    const formData = new FormData();
+    formData.append("file", data.user_image[0]);
+
     try {
-      const response = await postRegisterUser(data).unwrap();
-      console.log("Успешная регистрация:", response);
-    } catch (err) {
-      console.error("Ошибка регистрации:", err);
+      const { data: uploadFile } = await axios.post(
+        "https://api-crud.elcho.dev/api/v1/upload/file",
+        formData,
+         
+      );
+      const newData = {
+        user_image: uploadFile.url,
+        phone_number: data.phone_number,
+        username: data.username,
+        email: data.email,
+        password: data.password,
+      };
+
+      const userdata = postRegisterUser(newData)
+      console.log("Ответ сервера после регистрации:", userdata);
+    } catch (error: any) {
+      console.error("Ошибка при обработке запроса:", error);
+
+      if (error.response) {
+        console.error("Статус ошибки:", error.response.status);
+        console.error("Ответ сервера:", error.response.data);
+      } else if (error.request) {
+        console.error("Ошибка при запросе:", error.request);
+      } else {
+        console.error("Ошибка при настройке запроса:", error.message);
+      }
     }
   };
 
@@ -25,12 +53,28 @@ const Register = () => {
     <div>
       <h1>Register</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" {...register("username", { required: true })} />
-        <input type="text" {...register("email", { required: true })} />
-        <input type="text" {...register("password", { required: true })} />
-        <button type="submit" disabled={isLoading}>
-          {isLoading ? "Загрузка..." : "Зарегистрироваться"}
-        </button>
+        <input type="file" {...register("user_image", { required: true })} />
+        <input
+          type="text"
+          placeholder="Введите номер телефона"
+          {...register("phone_number", { required: true })}
+        />
+        <input
+          type="text"
+          placeholder="Введите имя пользователя"
+          {...register("username", { required: true })}
+        />
+        <input
+          type="email"
+          placeholder="Введите email"
+          {...register("email", { required: true })}
+        />
+        <input
+          type="password"
+          placeholder="Введите пароль"
+          {...register("password", { required: true })}
+        />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
